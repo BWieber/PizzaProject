4fb99106e118eabdc737e71b40ea5b9002117bdf
Brandon Wieber <brandon.m.wieber@gmail.com>
2016-03-04 09:48:18 -0500

    Added students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,24 @@
 class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.new(student_params)
+
+    if @student.save
+      flash[:notice] = "Student was saved successfully."
+      redirect_to student_index
+    else
+      flash[:error] = "There was an error. Please try again."
+      render :new
+    end
+
   end
 
   def update
@@ -13,4 +29,11 @@ class StudentsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :class)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+5.times do
+  user = User.create!(
+    email:        Faker::Internet.email,
+    password:     Faker::Internet.password,
+    confirmed_at: Time.now
+  )
+end
+users = User.all
+
+15.times do
+  student = Student.create!(
+    name:             Faker::Name.first_name,
+    classroom:        Faker::Name.first_name,
+  )
+end
+students = Student.all
+
+puts "Seed finished"
+puts "#{User.count} users created."
+puts "#{Student.count} topics created."
